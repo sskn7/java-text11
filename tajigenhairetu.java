9e855f7ea6c1d44d029d1ac30bc99e1ce3f1bdff
kana sasazawa <kana.yellow7@gmail.com>
2024-12-24 22:30:03 +0900

    多次元配列２

--- tajigenhairetu.java
+++ tajigenhairetu.java
@@ -9,6 +9,23 @@ public class tajigenhairetu {
 				{"フランス","ロシア"}
 		};
 		System.out.println(country[0][0]);
+		System.out.println(country[0][1]);
+		System.out.println(country[1][0]);
+		System.out.println(country[1][1]);
+		System.out.println(country[2][1]);
+		System.out.println(country[2][1]);
+		
+		//別パターンの書き方
+		String[][] country2 = new String[3][2];
+		
+		country2[0][0] = "日本";
+		country2[0][1] = "タイ";
+		country2[1][0] = "アメリカ";
+		country2[1][1] = "ブラジル";
+		country2[2][0] = "フランス";
+		country2[2][1] = "ロシア";
+		
+		System.out.println(country2[0][0]);
 
 	}
 
