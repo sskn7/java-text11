be8afc5b364dcfb0dbe2becc6faaef15c1710a74
kana sasazawa <kana.yellow7@gmail.com>
2024-12-24 22:21:57 +0900

    例文

---
+++ tajigenhairetu.java
@@ -0,0 +1,15 @@
+
+public class tajigenhairetu {
+
+	public static void main(String[] args) {
+		// 多次元配列とは配列の要素の中にさらに小さな箱を入れて管理する
+		String[][] country = {
+				{"日本","タイ"},
+				{"アメリカ","ブラジル"},
+				{"フランス","ロシア"}
+		};
+		System.out.println(country[0][0]);
+
+	}
+
+}
